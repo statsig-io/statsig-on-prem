17b8c7145bc0cdb2885b8dbef82e7abc946e8079
Callum Robertson <crobertson@atlassian.com>
2024-03-05 14:25:33 +1100

    export the TargetAppNames type from index

--- index.ts
+++ index.ts
@@ -3,12 +3,14 @@ import StatsigStorageExample from "./examples/StatsigStorageExample";
 import { StatsigInterface } from "./src/interfaces/StatsigInterface";
 import { StorageInterface } from "./src/interfaces/StorageInterface";
 import { ConfigSpecs } from "./src/types/ConfigSpecs";
+import { TargetAppNames } from "./src/types/TargetAppNames";
 
 export {
   StatsigStorageExample,
   StatsigInterface,
   StorageInterface,
-  ConfigSpecs
+  ConfigSpecs,
+  TargetAppNames
 };
 
 export default StatsigOnPrem
